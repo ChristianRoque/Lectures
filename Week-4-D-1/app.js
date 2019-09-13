544e333102944e60196348cd8300efe9f1a4d8ee
Christian Roque <croqu008@fiu.edu>
2019-09-13 07:26:29 -0400

    second lecture

--- Week-4-D-1/app.js
+++ Week-4-D-1/app.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res, next) => {
-	res.sendFile(__dirname + '/views/home-page.html');
+	let data = {
+		name: 'Ironhacker',
+		lastName: 'Rocking it!',
+		address: {
+			street: 'Your heart',
+			number: 87
+		},
+		cities: [ 'Miami', 'Madrid', 'Barcelona', 'Paris', 'México', 'Berlín' ]
+	};
+	res.render('index', data);
 });
 
 app.get('/cat', (req, res, next) => {
